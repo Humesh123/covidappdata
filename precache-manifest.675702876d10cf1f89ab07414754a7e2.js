8c865c997bdf5bb931279553b3fa50335dff707d
Humesh123 <humesh.sindpure@9abusiness.com>
2020-07-25 08:45:59 +0530

    Updates

---
+++ precache-manifest.675702876d10cf1f89ab07414754a7e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97444d5e26b9425b7aeab604534dc510",
+    "url": "/covidappdata/home/index.html"
+  },
+  {
+    "revision": "445dafc9f8e400b02191",
+    "url": "/covidappdata/home/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "11baaef32a20599e4940",
+    "url": "/covidappdata/home/static/css/main.06ff79c7.chunk.css"
+  },
+  {
+    "revision": "445dafc9f8e400b02191",
+    "url": "/covidappdata/home/static/js/2.f1f5d482.chunk.js"
+  },
+  {
+    "revision": "d9cca716f9d7cc1e1335be5d76cd259e",
+    "url": "/covidappdata/home/static/js/2.f1f5d482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11baaef32a20599e4940",
+    "url": "/covidappdata/home/static/js/main.7e712249.chunk.js"
+  },
+  {
+    "revision": "cba2f100f9ab01cfdd1f",
+    "url": "/covidappdata/home/static/js/runtime-main.f811aa2c.js"
+  },
+  {
+    "revision": "7326de8ecfc13404bf7ca78d83d2502f",
+    "url": "/covidappdata/home/static/media/covid.7326de8e.png"
+  }
+]);
